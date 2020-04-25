07315957ce23c7fcf0fcf895952c9b785749a868
takahiro.kuyama <takahiro.kuyama@gree.net>
2020-04-26 01:59:47 +0900

    基本を追加

--- simple-hello-world/index.js
+++ simple-hello-world/index.js
@@ -2,5 +2,17 @@ var app = new Vue({
    el: '#app',
    data: {
       message: 'Hello Vue!',
-   }
+      title: 'You loaded this page on ' + new Date().toLocaleString(),
+      seen: true,
+      todos: [
+         { text: 'first' },
+         { text: 'second' },
+         { text: 'three'},
+      ]
+   },
+   methods: {
+      reverseMessage: function () {
+         this.message = this.message.split('').reverse().join('')
+      }
+   },
 })
