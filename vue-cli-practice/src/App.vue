1c0de5e0a8ecdf6019b528be37208ac9f2a5d387
takahiro.kuyama <takahiro.kuyama@gree.net>
2020-05-06 14:35:09 +0900

    add todo page and create function

--- vue-cli-practice/src/App.vue
+++ vue-cli-practice/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/pages/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todo
   }
 }
 </script>
---
+++ vue-cli-practice/src/components/pages/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key=task.id>
+        {{ task.title }}
+      </li>
+    </ul>
+    <input v-model="nextTodo" placeholder="enter new task">
+    <input type="button" @click="addTodo" value="追加">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {},
+  data: function() {
+    return {
+      nextId: 1,
+      nextTodo: '',
+      tasks: [],
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.nextTodo)
+      if (this.nextTodo === '') {
+        return
+      }
+      const newTask = {id: this.nextId, title: this.nextTodo}
+      this.tasks.push(newTask)
+      this.nextTodo = ''
+      this.nextId += 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
